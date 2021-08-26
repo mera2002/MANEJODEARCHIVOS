59c361331da9f340da86f7b0bad4068621332872
mera2002 <SMIS072121@ugb.edu.sv>
2021-08-26 16:17:01 -0600

    archivos

---
+++ ManejoArchivos/ManejoArchivos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace ManejoArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir la ruta del archivo
+            string path = @"C:\ejemplo\datos.txt";
+            //crear un archivo, y agregar texto
+            using (StreamWriter sr = File.AppendText(path))
+
+                //evaluar si el archivo exite
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("El archivo ya existe!");
+
+                    //leer el contenido del archivo linea por linea
+                    String[] lines;
+                    lines = File.ReadAllLines(path);
+                    Console.WriteLine("CONTENIDO UTILIZANDO ReadAllLines()");
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        Console.WriteLine(lines[i]);
+                    }
+
+                    //leer el contenido del archivo
+                    Console.WriteLine("CONTENIDO UTILIZANDO ReadAllText()");
+                    String content;
+                    content = File.ReadAllText(path);
+                    Console.WriteLine(content);
+                }
+                else
+                {
+                    //crear un archivo, y agregar texto
+                    using (StreamWriter archivo = File.AppendText(path))
+                    {
+                        //contenido del archivo
+                        archivo.WriteLine("Programacion Computacional I");
+                        archivo.WriteLine("Clase teorica | Stream I/O");
+                        archivo.Close();//cierra el archivo creado
+                    }
+                }
+
+
+            Console.ReadKey();
+        }
+    }
+}
